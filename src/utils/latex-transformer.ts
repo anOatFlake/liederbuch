2449051ac290f6e6cc0d068a8bffa752ea30abd6
anOatFlake <annika@flock.cc>
2022-09-08 01:48:32 +0200

    added latexTransform methods and maex final html

---
+++ src/utils/latex-transformer.ts
@@ -0,0 +1,17 @@
+/**
+ * transforms latex to basic html
+ * @param fileString string
+ * @returns htmlString string
+ */
+export function latexToHtml(fileString: string): string {
+    return maexCustomLatexToHtml(fileString);
+}
+
+/**
+ * transforms the custom latex from maex's songfiles to html
+ * @param fileString string
+ * @returns htmlString string
+ */
+export function maexCustomLatexToHtml(fileString: string): string{
+    return "";
+}

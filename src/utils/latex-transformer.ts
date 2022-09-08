98c84f73b7dca4c599fcbd0ead0c79887ac86610
anOatFlake <annika@flock.cc>
2022-09-08 08:24:47 +0200

    updated latex-transform

--- src/utils/latex-transformer.ts
+++ src/utils/latex-transformer.ts
@@ -4,7 +4,13 @@
  * @returns htmlString string
  */
 export function latexToHtml(fileString: string): string {
-    return maexCustomLatexToHtml(fileString);
+    let tempString = "";
+    fileString.split(/\r?\n/).forEach(
+        line => {
+            tempString += maexCustomLatexToHtml(line);
+        }
+    )
+    return tempString;
 }
 
 /**
@@ -12,6 +18,42 @@ export function latexToHtml(fileString: string): string {
  * @param fileString string
  * @returns htmlString string
  */
-export function maexCustomLatexToHtml(fileString: string): string{
-    return "";
+function maexCustomLatexToHtml(fileLine: string): string {
+    if(fileLine.includes("\\documentclass") || fileLine.includes("\\begin{document}") || fileLine.includes("\\end{document}"))
+        return "";
+
+    if(fileLine.includes("\\song")) {
+        //title
+        fileLine.replace("\\song", "<div class='p-2'><p class='h2'>");  
+        //remove {} and language definition
+        fileLine.replace("{", "");
+        fileLine.replace("}", " ");
+        fileLine.replace("Deutsch", "");
+        //add endtag for title
+        fileLine += "</p></div>\n";
+    }
+
+    if(fileLine.includes("\\verse{")) 
+        fileLine.replace("\\verse{", '<div class="p-2 song">');
+    
+    if(fileLine.includes("\\chorus{")) 
+        fileLine.replace('\\chorus{', '<div class="part part-chorus">');
+    
+    if(fileLine.trim().includes("}"))
+        fileLine.replace("}", "</div>\n");
+    
+    if(fileLine.includes("\\li{")) {
+        fileLine.replace('\\li{', '<p class="line line-li">');
+        fileLine.replace('}', '</p>\n');
+        fileLine.replace('\\', ' <span class="chord" data-star="">');
+        fileLine.replace('[]', '</span>');
+    }
+
+    if(fileLine.includes("\\refrain"))
+        fileLine.replace('\\refrain', '<p class="line">Ref.</p>\n');
+
+    if(fileLine.includes("\\footer{"))
+        fileLine.replace('\\footer{', '<hr><br><div><div class="part-footer">');
+
+    return fileLine;
 }

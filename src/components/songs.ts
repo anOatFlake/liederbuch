ee7eb34fea3c4fda728191316e60064cca88d968
anOatFlake <annika@flock.cc>
2022-09-12 09:42:46 +0200

    added comments

--- src/components/songs.ts
+++ src/components/songs.ts
@@ -4,6 +4,10 @@ import { latexToHtml } from '../utils/latex-transformer';
 
 const songsDirectory = path.join(process.cwd(), 'src', 'songs');
 
+/**
+ * Gets all song ids (aka filenames) from the songsDirectory
+ * @returns object array with { params: { id: string } }
+ */
 export function getAllSongIds(): Array<{ params: { [key: string]: string } }> {
     const fileNames = fs.readdirSync(songsDirectory);
   
@@ -16,18 +20,32 @@ export function getAllSongIds(): Array<{ params: { [key: string]: string } }> {
     })
 }
 
+/**
+ * Gets all song ids (aka filenames) filterd by the first letters
+ * @param letter string
+ * @returns object array with { params: { id: string } }
+ */
 export function getAllSongIdsByFirstLetters(letter: string): Array<{ params: { [key: string]: string } }> {
     return getAllSongIds().filter((file) => {
       file.params.id?.startsWith(letter);
     })
 }
 
+/**
+ * Gets all song ids that start with a non letter character
+ * @returns  object array with { params: { id: string } }
+ */
 export function getAllSongIdsStartingWithNumberOrSymbol(): Array<{ params: { [key: string]: string } }> {
   return getAllSongIds().filter((file) => {
     /^\d/.test(file.params.id!) || /[$-/:-?{-~!"^_`\[\]]/.test(file.params.id!);
   })
 }
 
+/**
+ * Gets the songData from the id and transforms the fileContents to html
+ * @param id string | string[] | undefined
+ * @returns songdata object { id, contentHtml }
+ */
 export async function getSongData(id: string | string[] | undefined) {
   const fullPath = path.join(songsDirectory, `${id}.tex`);
   let fileContents = fs.readFileSync(fullPath, `utf-8`);

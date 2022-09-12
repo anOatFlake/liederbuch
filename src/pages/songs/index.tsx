6bda887ab7f76e08d9b01f0847de23901bb9ed3d
anOatFlake <annika@flock.cc>
2022-09-12 22:17:21 +0200

    added button row to songs-index

--- src/pages/songs/index.tsx
+++ src/pages/songs/index.tsx
@@ -7,6 +7,37 @@ const Songs: NextPage = ({songs}: any) => {
     return (
         <>
         <h1>Liste</h1>
+        
+        <div>
+            <button >A</button>
+            <button >B</button>
+            <button >C</button>
+            <button >D</button>
+            <button >E</button>
+            <button >F</button>
+            <button >G</button>
+            <button >H</button>
+            <button >I</button>
+            <button >J</button>
+            <button >K</button>
+            <button >L</button>
+            <button >M</button>
+            <button >N</button>
+            <button >O</button>
+            <button >P</button>
+            <button >Q</button>
+            <button >R</button>
+            <button >S</button>
+            <button >T</button>
+            <button >U</button>
+            <button >V</button>
+            <button >W</button>
+            <button >X</button>
+            <button >Y</button>
+            <button >Z</button>
+            <button >0-9</button>
+        </div>
+
         <ul>
             {songs.map((song: { params: {id: string}}) => (
                 <li>

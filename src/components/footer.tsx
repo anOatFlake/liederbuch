e2657bca6d8e6a7617e8bc3838db81648772945b
flockan71117 <flockan71117@th-nuernberg.de>
2022-09-18 18:55:27 +0200

    footer v1

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 const Footer = () => {
-  return <></>;
+  return (
+  <>  
+    <hr/>
+    <div className="p-2 justify-center">
+      <Link href="https://github.com/anOatFlake/liederbuch">
+        <a>
+          Github Repo
+        </a>
+      </Link>
+    </div>
+  </>
+  );
 };
 
 export default Footer;

bbfbad6701d2c17f570ac3a8a45cfbcf23d4b7c3
anOatFlake <annika@flock.cc>
2022-09-20 15:06:11 +0200

    local sqlite db

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from "next";
-import Head from "next/head";
+import { useSession } from "next-auth/react";
+import { trpc } from '../utils/trpc'
 
 const Home: NextPage = () => {
-  return <>wip</>;
+  const { data: session } = useSession();
+  return (
+    <>
+    
+    </>
+  );
 };
 
 export default Home;

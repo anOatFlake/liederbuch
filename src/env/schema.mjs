ed4eeea226d5297888818ad9e259fa956ba246f1
anOatFlake <annika@flock.cc>
2022-09-27 19:52:09 +0200

    server schema

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,7 +6,12 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  // FOO: z.string(),
+  DATABASE_URL: z.string().url(),
+  NODE_ENV: z.enum(["development", "test", "production"]),
+  NEXTAUTH_SECRET: z.string(),
+  NEXTAUTH_URL: z.string().url(),
+  EMAIL_SERVER: z.string(),
+  EMAIL_FROM: z.string(),
 });
 
 /**

f06755ec9e8356376fad665fa6c8ea69c77f9a5d
anOatFlake <annika@flock.cc>
2022-11-09 16:58:45 +0100

    nav styling changes (wip)

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -32,27 +32,48 @@ const SideBar: React.FC = () => {
   ];
   return (
     <>
-      <nav className="">
         <div className="fixed top-0 w-full p-4 border-b-2 border-teal-700 md:hidden">
-          <button>Insert menu icon</button>
+          <button id="nav-toggle" type="button" aria-pressed="false">Insert menu icon</button>
         </div>
-        <div>
-          
-        </div>
+        <nav className="md:hidden w-full fixed top-16 bottom-0">
+          <div className="px-8 mt-6 w-full">
+              <Link href={"/currentSong"}>Folgen</Link>
+            </div>
+            <div className="px-8 mt-6 w-full">
+              <Link href={"/profile"}>Profil</Link>
+            </div>
+            <div className="px-8 mt-12 w-full">
+              <Link href={"/songs"}>Liste</Link>
+            </div>
+            <div className="grid grid-cols-6 px-8 mt-1 w-screen">
+              {letters.map((letter: string) => (
+                <button key={letter} className="p-2 text-center">
+                  <Link
+                    href={{
+                      pathname: "/songs",
+                      query: { letter: letter },
+                    }}
+                  >
+                    {letter}
+                  </Link>
+                </button>
+              ))}
+            </div>
+        </nav>
 
-        <div className="fixed top-0 hidden h-full p-4 border-r-2 border-teal-700 md:block w-52">
-          <div className="px-4 mt-4">
+        <nav className="fixed top-0 hidden h-full p-4 border-r-2 border-teal-700 md:block w-64">
+          <div className="px-4 mt-6">
             <Link href={"/currentSong"}>Folgen</Link>
           </div>
-          <div className="px-4 mt-2">
+          <div className="px-4 mt-6">
             <Link href={"/profile"}>Profil</Link>
           </div>
-          <div className="px-4 mt-6">
+          <div className="px-4 mt-12">
             <Link href={"/songs"}>Liste</Link>
           </div>
           <div className="grid grid-cols-6 px-2 mt-1">
             {letters.map((letter: string) => (
-              <button key={letter} className="p-1 text-center">
+              <button key={letter} className="p-2 text-center">
                 <Link
                   href={{
                     pathname: "/songs",
@@ -64,8 +85,7 @@ const SideBar: React.FC = () => {
               </button>
             ))}
           </div>
-        </div>
-      </nav>
+        </nav>
     </>
   );
 };

f89449c75b20ae59d2a238b156645864e1e60c21
anOatFlake <annika@flock.cc>
2022-11-13 21:42:02 +0100

    updated prisma

---
+++ prisma/migrations/20221113204111_/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Example` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Example";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "Repertoire" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "reportoireName" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "inviteCode" TEXT,
+    CONSTRAINT "Repertoire_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Repertoire_inviteCode_key" ON "Repertoire"("inviteCode");

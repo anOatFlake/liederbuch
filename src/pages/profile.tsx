db815a459bf605517fe1242840e4159383c1c413
AnOatFlake <annika@flock.cc>
2022-11-17 21:26:17 +0100

    general profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,25 @@
-const Profile = () => {
-  return <></>;
+import type { NextPage } from "next";
+import Head from "next/head";
+import SideBar from "../components/sidebar";
+
+const Profile: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Profil</title>
+        <meta name="profile" content="Profile of the logged in user" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SideBar />
+      <main>
+        
+      </main>
+    </>
+  );
 };
 
 export default Profile;
+
+// Image - Name
+// mail address
+// Reportaire

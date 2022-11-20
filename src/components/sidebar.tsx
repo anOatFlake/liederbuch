be5c5b7f0e08c3c4fc4a58ecf8972ea5e1988756
anOatFlake <annika@flock.cc>
2022-11-20 14:46:36 +0100

    mobile nav (wip)

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
  */
 const SideBar: React.FC = () => {
   const { data: sessionData } = useSession();
+  let expanded = false;
 
   const letters = [
     "A",
@@ -36,52 +37,70 @@ const SideBar: React.FC = () => {
     "Z",
     "0-9",
   ];
+
   return (
     <>
       <div className="fixed top-0 w-full border-b-2 border-teal-500 p-4 dark:border-teal-700 md:hidden">
-        <button id="nav-toggle" type="button" aria-pressed="false">
+        <button
+          id="nav-toggle"
+          type="button"
+          onClick={() => {
+            expanded = !expanded;
+          }}
+        >
           Insert menu icon
         </button>
       </div>
-      <nav className="fixed bottom-0 top-16 w-full md:hidden">
-        <div className="mt-6 w-full px-8 tracking-widest underline-offset-4 hover:underline">
-          <Link href={"/currentSong"}>Folgen</Link>
-        </div>
-        {sessionData ? (
-          <>
-            <div className="mt-6 w-full px-8 tracking-widest underline-offset-4 hover:underline">
-              <Link href={"/profile"}>Profil</Link>
-            </div>
-            <div className="mt-6 w-full  tracking-widest underline-offset-4 hover:underline">
-              <Link href={"/"} onClick={() => signOut({ callbackUrl: 'http://localhost:3000/' })}>Logout</Link>
-            </div>
-          </>
-        ) : (
-          <div className="mt-6 w-full  tracking-widest underline-offset-4 hover:underline">
-            <Link href={"/"} onClick={() => signIn("discord")}>Login</Link>
+      {true ?? (
+        <nav className={"fixed bottom-0 top-16 w-full md:hidden"}>
+          <div className="mt-6 w-full px-8 tracking-widest underline-offset-4 hover:underline">
+            <Link href={"/currentSong"}>Folgen</Link>
           </div>
-        )}
-        <div className="mt-12 w-full px-8 tracking-widest underline-offset-4 hover:underline">
-          <Link href={"/songs"}>Liste</Link>
-        </div>
-        <div className="mt-1 grid w-screen grid-cols-6 px-8">
-          {letters.map((letter: string) => (
-            <button
-              key={letter}
-              className="p-2 text-center underline-offset-4 hover:underline"
-            >
-              <Link
-                href={{
-                  pathname: "/songs",
-                  query: { letter: letter },
-                }}
-              >
-                {letter}
+          {sessionData ? (
+            <>
+              <div className="mt-6 w-full px-8 tracking-widest underline-offset-4 hover:underline">
+                <Link href={"/profile"}>Profil</Link>
+              </div>
+              <div className="mt-6 w-full px-8 tracking-widest underline-offset-4 hover:underline">
+                <Link
+                  href={"/"}
+                  onClick={() =>
+                    signOut({ callbackUrl: "http://localhost:3000/" })
+                  }
+                >
+                  Logout
+                </Link>
+              </div>
+            </>
+          ) : (
+            <div className="mt-6 w-full px-8 tracking-widest underline-offset-4 hover:underline">
+              <Link href={"/"} onClick={() => signIn("discord")}>
+                Login
               </Link>
-            </button>
-          ))}
-        </div>
-      </nav>
+            </div>
+          )}
+          <div className="mt-12 w-full px-8 tracking-widest underline-offset-4 hover:underline">
+            <Link href={"/songs"}>Liste</Link>
+          </div>
+          <div className="mt-1 grid w-screen grid-cols-6 px-8">
+            {letters.map((letter: string) => (
+              <button
+                key={letter}
+                className="p-2 text-center underline-offset-4 hover:underline"
+              >
+                <Link
+                  href={{
+                    pathname: "/songs",
+                    query: { letter: letter },
+                  }}
+                >
+                  {letter}
+                </Link>
+              </button>
+            ))}
+          </div>
+        </nav>
+      )}
 
       <nav className="fixed top-0 hidden h-full w-64 border-r-2 border-teal-500 p-4 dark:border-teal-700 md:block">
         <div className="mt-6 px-4 tracking-widest underline-offset-4 hover:underline">
@@ -93,12 +112,21 @@ const SideBar: React.FC = () => {
               <Link href={"/profile"}>Profil</Link>
             </div>
             <div className="mt-6 w-full px-4 tracking-widest underline-offset-4 hover:underline">
-              <Link href={"/"} onClick={() => signOut({ callbackUrl: 'http://localhost:3000/' })}>Logout</Link>
+              <Link
+                href={"/"}
+                onClick={() =>
+                  signOut({ callbackUrl: "http://localhost:3000/" })
+                }
+              >
+                Logout
+              </Link>
             </div>
           </>
         ) : (
           <div className="mt-6 w-full px-4 tracking-widest underline-offset-4 hover:underline">
-            <Link href={"/"} onClick={() => signIn("discord")}>Login</Link>
+            <Link href={"/"} onClick={() => signIn("discord")}>
+              Login
+            </Link>
           </div>
         )}
         <div className="mt-12 px-4 tracking-widest underline-offset-4 hover:underline">

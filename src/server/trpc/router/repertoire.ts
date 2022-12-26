7a60336049180a3c8ab47d95a74deff2a0b8466e
AnOatFlake <annika@flock.cc>
2022-12-26 22:16:53 +0100

    added mutation for songs

--- src/server/trpc/router/repertoire.ts
+++ src/server/trpc/router/repertoire.ts
@@ -1,12 +1,8 @@
+import { contextProps } from "@trpc/react-query/dist/internals/context";
 import { router, publicProcedure, protectedProcedure } from "../trpc";
 import { z } from "zod";
 
 export const repertoireRouter = router({
-
-  getSession: publicProcedure.query(({ ctx }) => {
-    return ctx.session;
-  }),
-
   getRepertoire: publicProcedure.input(z.string()).query(({ ctx, input }) => {
     return ctx.prisma.repertoire.findUnique({
         where: {
@@ -14,7 +10,6 @@ export const repertoireRouter = router({
         }
     })
   }),
-  
 
   isSongInRepertoire: protectedProcedure.input(z.string()).query(({ input, ctx }) => {
     return ctx.prisma.repertoire.findFirst({
@@ -24,5 +19,38 @@ export const repertoireRouter = router({
         }
       }
     }) ?? false;
+  }),
+
+  addSongToRepertoire: protectedProcedure.input(z.string()).mutation(async ({ input, ctx }) => {
+    const currentRep = await ctx.prisma.repertoire.findUnique({
+      where: {
+        userId: ctx.session.user.id,
+      }
+    })
+    const result = await ctx.prisma.repertoire.update({
+      where: {
+        userId: ctx.session.user.id,
+      },
+      data: {
+        songs: input + ', ' + currentRep?.songs
+      }
+    })
+    return { added: result }
+  }),
+  removeSongFromRepertoire: protectedProcedure.input(z.string()).mutation(async ({ input, ctx }) => {
+    const currentRep = await ctx.prisma.repertoire.findUnique({
+      where: {
+        userId: ctx.session.user.id,
+      }
+    })
+    const updatedSongs = currentRep?.songs?.replace(input + ', ', '')
+    ctx.prisma.repertoire.update({
+      where: {
+        userId: ctx.session.user.id,
+      },
+      data: {
+        songs: updatedSongs
+      }
+    })
   })
 });

7940a5cad60f73e7e85e7c9073b3d956718acc58
AnOatFlake <annika@flock.cc>
2022-12-27 22:27:54 +0100

    updated statemanagement

--- src/components/buttons/addToRepButton.tsx
+++ src/components/buttons/addToRepButton.tsx
@@ -1,28 +1,36 @@
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 import { trpc } from "../../utils/trpc";
 
 const AddToRepButton: React.FC<{ id: string }> = ({ id }) => {
   const { data: sessionData } = useSession();
 
-  const { data: isInReporoire } =
-    trpc.repertoire.isSongInRepertoire.useQuery(id);
+  const [isInReporoire, setIsInRepertoire] = useState(
+    trpc.repertoire.isSongInRepertoire.useQuery(id)
+  );
+
   const addSong = trpc.repertoire.addSongToRepertoire.useMutation();
   const removeSong = trpc.repertoire.removeSongFromRepertoire.useMutation();
 
-  //if logged in
   return sessionData ? (
     !isInReporoire ? (
       //TODO: Button styling
       <button
         className="inline-block h-6 w-6 rounded-full bg-cyan-700"
-        onClick={() => addSong.mutate(id)}
+        onClick={() => {
+          addSong.mutate(id);
+          setIsInRepertoire(trpc.repertoire.isSongInRepertoire.useQuery(id));
+        }}
       >
         +
       </button>
     ) : (
       <button
         className="inline-block h-6 w-6 rounded-full bg-teal-700"
-        onClick={() => removeSong.mutate(id)}
+        onClick={() => {
+          removeSong.mutate(id);
+          setIsInRepertoire(trpc.repertoire.isSongInRepertoire.useQuery(id));
+        }}
       >
         -
       </button>

486061d433a0c10277d134d50fb65a2ab43d00c6
AnOatFlake <annika@flock.cc>
2022-12-27 09:56:39 +0100

    update button functionality

--- src/components/buttons/addToRepButton.tsx
+++ src/components/buttons/addToRepButton.tsx
@@ -1,26 +1,29 @@
 import { useSession } from "next-auth/react";
 import { trpc } from "../../utils/trpc";
 
-const AddToRepButton: React.FC<{id: string}> = ({id}) => {
+const AddToRepButton: React.FC<{ id: string }> = ({ id }) => {
   const { data: sessionData } = useSession();
 
-  const { data: isInReporoire } = trpc.repertoire.isSongInRepertoire.useQuery(id)
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  //@ts-ignore
-  const { addMutation } = trpc.repertoire.addSongToRepertoire.useMutation()
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  //@ts-ignore
-  const { removeMutation } = trpc.repertoire.removeSongFromRepertoire.useMutation()
+  const { data: isInReporoire } =
+    trpc.repertoire.isSongInRepertoire.useQuery(id);
+  const addSong = trpc.repertoire.addSongToRepertoire.useMutation();
+  const removeSong = trpc.repertoire.removeSongFromRepertoire.useMutation();
 
   //if logged in
   return sessionData ? (
     !isInReporoire ? (
       //TODO: Button styling
-      <button className="bg-cyan-700 rounded-full inline-block w-6 h-6" onClick={() => addMutation(id)}>
+      <button
+        className="inline-block h-6 w-6 rounded-full bg-cyan-700"
+        onClick={() => addSong.mutate(id)}
+      >
         +
       </button>
     ) : (
-      <button className="bg-teal-700 rounded-full inline-block w-6 h-6" onClick={() => removeMutation(id)}>
+      <button
+        className="inline-block h-6 w-6 rounded-full bg-teal-700"
+        onClick={() => removeSong.mutate(id)}
+      >
         -
       </button>
     )

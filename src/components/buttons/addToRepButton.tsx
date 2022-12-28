c3797ea07d8e2ae01796f42e404664384f603ff0
AnOatFlake <annika@flock.cc>
2022-12-28 17:34:53 +0100

    state handeling from buttons

--- src/components/buttons/addToRepButton.tsx
+++ src/components/buttons/addToRepButton.tsx
@@ -1,24 +1,27 @@
 import { useSession } from "next-auth/react";
 import { trpc } from "../../utils/trpc";
 import { isSongInRepertoire } from "../../utils/repertoire";
+import { useState } from "react";
 
 const AddToRepButton: React.FC<{ id: string }> = ({ id }) => {
   const { data: sessionData } = useSession();
 
   const { data: songData } = trpc.users.getSongs.useQuery();
-  let isInReporoire = isSongInRepertoire(id, songData);
+  const [isInReperoire, setIsInRepertoire] = useState(
+    isSongInRepertoire(id, songData)
+  );
 
   const addSong = trpc.repertoire.addSongToRepertoire.useMutation();
   const removeSong = trpc.repertoire.removeSongFromRepertoire.useMutation();
 
   return sessionData ? (
-    !isInReporoire ? (
+    !isInReperoire ? (
       //TODO: Button styling --> Icons https://github.com/Templarian/MaterialDesign or https://github.com/feathericons/feather
       <button
         className="inline-block h-6 w-6 rounded-full bg-cyan-700"
         onClick={() => {
           addSong.mutate(id);
-          isInReporoire = true;
+          setIsInRepertoire(true);
         }}
       >
         +
@@ -28,7 +31,7 @@ const AddToRepButton: React.FC<{ id: string }> = ({ id }) => {
         className="inline-block h-6 w-6 rounded-full bg-teal-700"
         onClick={() => {
           removeSong.mutate(id);
-          isInReporoire = false;
+          setIsInRepertoire(false);
         }}
       >
         -

799ab1c5e3ef01fbb3de258681c5de3b1dd74461
AnOatFlake <annika@flock.cc>
2022-12-29 16:21:31 +0100

    prettier

--- src/utils/repertoire.ts
+++ src/utils/repertoire.ts
@@ -1,6 +1,5 @@
 /**
  * Helper function to determine if the song is already saved in the reportoire
- * 
  * @param songId
  * @param allSongs
  * @returns boolean
@@ -14,10 +13,9 @@ export function isSongInRepertoire(
 
 /**
  * Helper function to split the reportoire string into a string array
- * 
- * @param allSongs 
+ * @param allSongs
  * @returns string[]
  */
 export function repertoireAsArray(allSongs: string | null | undefined) {
-    return allSongs?.split(', ');
+  return allSongs?.split(", ");
 }

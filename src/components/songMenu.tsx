940a1e3810152055b69378e85bdf8f1e58f8c8cc
AnOatFlake <annika@flock.cc>
2022-12-29 11:43:28 +0100

    added menu for player in songview

---
+++ src/components/songMenu.tsx
@@ -0,0 +1,24 @@
+import { useSession } from "next-auth/react";
+import AddToRepButton from "./buttons/addToRepButton";
+import CurrentSongButton from "./buttons/currentSongButton";
+
+const SongMenu: React.FC<{ id: string }> = ({ id }) => {
+  const { data: sessionData } = useSession();
+  // TODO styling va mobile styling
+  return true ? (
+    <div className="m-4 flex rounded-r-sm border-l-4 border-slate-700 bg-slate-100 p-4 font-semibold dark:bg-slate-800/80">
+      <div className="container">
+        Zum Repertoire hinzufügen/entfernen:
+        <AddToRepButton id={id} />
+      </div>
+      <div className="container">
+        Mit Sängern teilen: (WIP)
+        <CurrentSongButton />
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
+};
+
+export default SongMenu;

8847666a358f1c3c276d9e0c8d2024be69fa8f74
AnOatFlake <annika@flock.cc>
2022-12-30 22:09:16 +0100

    deploy fix

--- src/pages/currentSong.tsx
+++ src/pages/currentSong.tsx
@@ -1,10 +1,30 @@
-import Login from "../components/login";
+import Head from "next/head";
+import SideBar from "../components/sidebar";
 
 const CurrentSong = () => {
+  const loggedIn = true; // TODO: Login request
+
   return (
-    <>
-      <Login />
-    </>
+    !loggedIn ? (
+      <>
+      </>
+    ) :
+      (
+      <>
+      <Head>
+        <title>{}</title>
+        <meta
+          name="description"
+          content="Songtext for the song {songData.id}"
+        ></meta>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SideBar />
+      <main>
+       
+      </main>
+      </>
+      )
   );
 };
 

6a72cf6d7212c62398f0f2f06863daec530e315c
AnOatFlake <annika@flock.cc>
2023-01-12 12:05:48 +0100

    added add and remove icon

--- src/components/buttons/addToRepButton.tsx
+++ src/components/buttons/addToRepButton.tsx
@@ -10,32 +10,33 @@ const AddToRepButton: React.FC<{ id: string }> = ({ id }) => {
   const [isInReperoire, setIsInRepertoire] = useState(
     isSongInRepertoire(id, songData)
   );
-  useEffect(() => {}, [isInReperoire]);
 
   const addSong = trpc.repertoire.addSongToRepertoire.useMutation();
   const removeSong = trpc.repertoire.removeSongFromRepertoire.useMutation();
 
-  return sessionData ? (
+  return true ? (
     !isInReperoire ? (
       //TODO: Button styling --> Icons https://github.com/Templarian/MaterialDesign or https://github.com/feathericons/feather
       <button
+        id={id}
         className="inline-block h-6 w-6 rounded-full border-2 bg-teal-400 hover:border-teal-600 dark:border-slate-900 dark:bg-teal-900"
         onClick={() => {
           addSong.mutate(id);
           setIsInRepertoire(true);
         }}
       >
-        ADD
+        <img src="/icons/plus.svg" alt="addIcon"></img>
       </button>
     ) : (
       <button
+        id={id}
         className="inline-block h-6 w-6 rounded-full border-2 bg-red-300 hover:border-red-500 dark:border-slate-900 dark:bg-red-900"
         onClick={() => {
           removeSong.mutate(id);
           setIsInRepertoire(false);
         }}
       >
-        REMOVE
+      <img src="/icons/x-mark.svg" alt="removeIcon"></img>
       </button>
     )
   ) : (

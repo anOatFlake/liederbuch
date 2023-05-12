0e225efb11d6aaec89dc8d200ce04ef403f000c7
anOatFlake <annika@flock.cc>
2023-05-12 14:16:56 +0200

    fixed tailwind styles

--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  tailwindConfig: "./tailwind.config.js",
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
